1720d3a8415171e5293367e6cbe4ef13dc04596f
Jdkruege <jrkuege@iastate.edu>
2015-02-21 14:02:53 -0600

    Follow script updated
    This includes the follow script.... yeah

---
+++ Game Jam 2015/Assets/Scripts/FollowScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+	public int distanceAway;
+	public bool turnWithPlayer;
+	private bool lookingLeft = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update(){
+
+        Vector3 PlayerPOS = GameObject.Find("BasePerson").transform.transform.position;
+        transform.position = new Vector3(PlayerPOS.x, PlayerPOS.y, PlayerPOS.z - distanceAway);
+
+		if (turnWithPlayer) {
+			Vector3 PlayerRot = GameObject.Find ("BasePerson").transform.rotation.eulerAngles;
+			if(((int)PlayerRot.y == 180) && !lookingLeft)
+			{
+				transform.Rotate(new Vector3(0, -90, 0));
+				lookingLeft = true;
+			}
+			if(((int)PlayerRot.y == 0) && lookingLeft)
+			{
+				transform.Rotate(new Vector3(0, 90, 0));
+				lookingLeft = false;
+			}
+		}
+ }
+}
