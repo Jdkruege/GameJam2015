d9b94149a0407333d477bed12d8b607221bb4fe5
Jdkruege <jrkuege@iastate.edu>
2015-02-20 23:12:26 -0600

    Guy moves
    Got a guy moving around, and he can trigger a thing. Pretty good so far

---
+++ Game Jam 2015/Assets/BreakScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakScript : MonoBehaviour {
+
+    void Start()
+    {
+    }
+
+    void OnTriggerEnter2D()
+    {
+        Destroy(GameObject.Find("FallthroughBricks"));
+    }
+}
---
+++ Game Jam 2015/Assets/Camera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update(){
+
+        int DistanceAway = 10;
+        Vector3 PlayerPOS = GameObject.Find("BasePerson").transform.transform.position;
+        GameObject.Find("Main Camera").transform.position = new Vector3(PlayerPOS.x, PlayerPOS.y, PlayerPOS.z - DistanceAway);
+ 
+ }
+}
